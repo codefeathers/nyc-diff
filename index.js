2380d5983df58bbc303a1aee5a4d5da91e37b440
Shashank Kaul <kaulshashank96@gmail.com>
2020-03-09 00:19:04 +0530

    (nyc) .nyc_output processing.

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const processDiff = () => {
 		return new Promise((resolve, reject) => {
 			const map = [];
 			fs.createReadStream(DIFF_PATH)
-				.on('data', function (chunk) {
+				.on('data', chunk => {
 					chunk = chunk.toString();
 					const lines = chunk.split("\n");
 					if (chunk && lines && lines[0] && lines[0] !== "") {
@@ -67,15 +67,71 @@ const processDiff = () => {
 						}
 					}
 				})
-				.on("end", function () {
+				.on("end", () => {
 					resolve(map);
 				})
-				.on("error", function (err) {
+				.on("error", err => {
 					reject(err);
 				});
 		});
 	} else {
 		return Promise.reject(new Error("No diff found."))
+	}
+}
+
+const compareDiff = map => {
+	if (map.length == 0) {
+		resolve([]);
+	}
+	const nycOutputPath = WORKING_DIR + "/.nyc_output";
+	const nycIndexPath = nycOutputPath + "/processinfo/index.json";
+
+	if (fs.existsSync(nycIndexPath)) {
+
+		// eslint-disable-next-line security/detect-non-literal-require
+		const nycFiles = require(nycIndexPath).files;
+
+		const diffResultMap = [];
+
+		if (nycFiles) {
+			map.forEach(gitFile => {
+				const [filePathFromProjectDir, linesFromGitDiff] = gitFile;
+				const completeFilePath = WORKING_DIR + "/" + filePathFromProjectDir;
+
+				let found = false;
+				for (const nycFile in nycFiles) {
+					if (nycFile === completeFilePath) {
+						found = true;
+						const nyc = require(nycOutputPath + nycFiles[nycFile]);
+						if (nyc && nyc[nycFile]) {
+							const { statementMap, s } = nyc[nycFile];
+							const uncoveredLines = [];
+							linesFromGitDiff.forEach(l => {
+								for (const statement in statementMap) {
+									if (statementMap[statement] != null
+										&& statementMap[statement].start
+										&& statementMap[statement].end
+										&& (statementMap[statement].start.line <= l && statementMap[statement].end.line >= l)) {
+										if (s[statement] === 0) { // Not covered
+											uncoveredLines.push(l);
+											break;
+										}
+									}
+								}
+							});
+							if (uncoveredLines.length) diffResultMap.push([filePathFromProjectDir, uncoveredLines]);
+						}
+					}
+				}
+				if (!found) console.log(`${filePathFromProjectDir} not found in nyc output data`);
+			});
+
+			return resolve(diffResultMap);
+		} else {
+			exitWithFailure("`.nyc_output/processinfo/index.json` file does not contain the `files` object.")();
+		}
+	} else {
+		exitWithFailure("`.nyc_output/processinfo/index.json` file was not found in predefined location.")();
 	}
 }
 
@@ -119,6 +175,7 @@ process.stdin.on("data", data => {
 
 process.stdin.on("close", () => {
 	processDiff()
+		.then(compareDiff)
 		.catch(exitWithFailure("No diff found."));
 });
 
