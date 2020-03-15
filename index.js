2fa973317918cd3dc6e3d4fbf585fd5c426d99ff
Shashank Kaul <kaulshashank96@gmail.com>
2020-03-15 18:03:42 +0530

    (nyc-diff) Working version.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/node
+#! /usr/bin/env node
 
 const fs = require("fs");
 
@@ -8,14 +8,29 @@ const diffLineNumbers = require("./tools/diff-line-numbers");
 const parseDiff = require("./tools/parse-diff");
 const compareDiff = require("./tools/compare-diff");
 
-const NYC_OUTPUT_LOCATION = process.env.NYC_OUTPUT_LOCATION;
+const NYC_OUTPUT_LOCATION = process.env.NYCDIFF_NYC_OUTPUT_LOCATION;
+
+if (!NYC_OUTPUT_LOCATION) {
+	exitWithFailure("Please provide the location where nyc is being run using the `NYC_OUTPUT_LOCATION` environment variable.\n Or, check usage on https://github.com/codefeathers/nyc-diff.")()
+}
+
+const RESULT_PATH = process.env.NYCDIFF_RESULT_PATH
+	? process.env.NYCDIFF_RESULT_PATH.endsWith("json")
+		? process.env.NYCDIFF_RESULT_PATH
+		: process.env.NYCDIFF_RESULT_PATH + `/nyc-diff-${Date.now()}.json`
+	: NYC_OUTPUT_LOCATION + `/nyc-diff-${Date.now()}.json`;
 
 const DIFF_LOCATION = __dirname + "/branch.diff";
 
-diffLineNumbers(process.stdin, fs.createWriteStream(DIFF_LOCATION), () => {
-	parseDiff(DIFF_LOCATION)
-		.then(compareDiff.bind(undefined, NYC_OUTPUT_LOCATION))
-		.then(console.log.bind(undefined, "Result: "))
-		.then(exitWithSuccess())
-		.catch(exitWithFailure("An error occured!"));
-});
+diffLineNumbers(process.stdin, fs.createWriteStream(DIFF_LOCATION))
+	.then(parseDiff.bind(undefined, DIFF_LOCATION))
+	.then(compareDiff.bind(undefined, NYC_OUTPUT_LOCATION))
+	.then(result => {
+		if (result && result.length) {
+			fs.writeFileSync(RESULT_PATH, JSON.stringify(result, null, 4));
+			exitWithSuccess(`diff has been written to ${RESULT_PATH}`, 1)();
+		} else {
+			exitWithSuccess("No diff was calculated.")();
+		}
+	})
+	.catch(exitWithFailure("An unexpected error occured!"));
--- tools/diff-line-numbers.js
+++ tools/diff-line-numbers.js
@@ -1,67 +1,71 @@
-module.exports = (inputStream, outputStream, completionCallback) => {
-	const state = {
-		watch: false,
-		curr: {
-			src: 0,
-			dest: 0,
-		},
-	};
+module.exports = (inputStream, outputStream) => {
+	return new Promise((resolve, reject) => {
+		const state = {
+			watch: false,
+			curr: {
+				src: 0,
+				dest: 0,
+			},
+		};
 
-	const write = (...segs) => outputStream.write(segs.join("") + "\n");
+		const write = (...segs) => outputStream.write(segs.join("") + "\n");
 
-	const convertAndLog = {
-		src: line => {
-			write(String(state.curr.src).padEnd(4, " "), ":", line);
-			++state.curr.src;
-		},
-		dest: line => {
-			write(String(state.curr.dest).padEnd(4, " "), ":", line);
-			++state.curr.dest;
-		},
-		neutral: line => {
-			write(String(state.curr.dest).padEnd(4, " "), ":", line);
-			++state.curr.dest;
-			++state.curr.src;
-		},
-	};
+		const convertAndLog = {
+			src: line => {
+				write(String(state.curr.src).padEnd(4, " "), ":", line);
+				++state.curr.src;
+			},
+			dest: line => {
+				write(String(state.curr.dest).padEnd(4, " "), ":", line);
+				++state.curr.dest;
+			},
+			neutral: line => {
+				write(String(state.curr.dest).padEnd(4, " "), ":", line);
+				++state.curr.dest;
+				++state.curr.src;
+			},
+		};
 
-	inputStream.on("data", data => {
-		const lines = String(data).split("\n");
-		lines.forEach(line => {
-			if (line.startsWith("@@")) {
-				const [src, dest] = line
-					.split(" ")
-					.filter(x => x !== "@@")
-					// Remove +, -
-					.map(x => x.slice(1))
-					// Split to components
-					.map(x => x.split(","))
-					// parse as Number
-					.map(([start, end]) => ({
-						start: Number(start),
-						end: Number(end),
-					}));
-				state.watch = true;
-				// Reset counter to reflect current context
-				Object.assign(state.curr, { src: src.start, dest: dest.start });
-				return write(line);
-			} else if (state.watch) {
-				if (line.startsWith(" ")) {
-					convertAndLog.neutral(line);
-				}
-				if (line.startsWith("+")) {
-					convertAndLog.dest(line);
-				}
-				if (line.startsWith("-")) {
-					convertAndLog.src(line);
-				}
-				if (line.startsWith("diff")) {
-					state.watch = false;
-					write(line);
-				}
-			} else write(line);
+		inputStream.on("data", data => {
+			const lines = String(data).split("\n");
+			lines.forEach(line => {
+				if (line.startsWith("@@")) {
+					const [src, dest] = line
+						.split(" ")
+						.filter(x => x !== "@@")
+						// Remove +, -
+						.map(x => x.slice(1))
+						// Split to components
+						.map(x => x.split(","))
+						// parse as Number
+						.map(([start, end]) => ({
+							start: Number(start),
+							end: Number(end),
+						}));
+					state.watch = true;
+					// Reset counter to reflect current context
+					Object.assign(state.curr, { src: src.start, dest: dest.start });
+					return write(line);
+				} else if (state.watch) {
+					if (line.startsWith(" ")) {
+						convertAndLog.neutral(line);
+					}
+					if (line.startsWith("+")) {
+						convertAndLog.dest(line);
+					}
+					if (line.startsWith("-")) {
+						convertAndLog.src(line);
+					}
+					if (line.startsWith("diff")) {
+						state.watch = false;
+						write(line);
+					}
+				} else write(line);
+			});
 		});
-	});
 
-	inputStream.on("end", completionCallback);
+		inputStream.on("error", reject);
+
+		inputStream.on("end", resolve);
+	});
 };
